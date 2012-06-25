890563de67b0d56cb5bbde7d8f1bde52ffbcdc60
Andrew Eells <aeells@qmetric.co.uk>
2012-06-25 14:14:18 +0100

    Having trouble with setup requires and dependencies. Resetting.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='agile-database',
       version='0.0.1',
       packages=find_packages(),
       scripts=['bin/agile_database.py'],
-      requires=["mock", "PyYAML", "MySQLdb"],
+      requires=[],
       test_suite='tests',
       tests_require='mock',
       )
